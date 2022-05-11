df45e640c7b8d64a715df1fd58135259b8e75cba
sdmnny <sdmnny@gmail.com>
2022-05-11 14:51:39 -0400

    added psuedocode

--- assests/script.js
+++ assests/script.js
@@ -9,3 +9,54 @@ var computerRecord = {
 };
 
 var ties = 0;
+
+window.alert("DO YOU WANT TO PLAY A GAME?")
+
+var userInput= prompt("Enter Rock(R), Paper(P), or Scissors (S)");
+
+
+
+var computerInput = prompt(Math.random)
+
+
+
+// state:
+//play again?
+// player wins, losses, ties
+
+// play loop 
+
+// state:
+// user choice 
+// computer choice
+// winner 
+
+
+// prompting the user for choice of R P and S 
+// func: validate the user input 
+
+// alert "user choice ____"
+
+// generate the computer choice 
+
+// alert "computer choice ____"
+
+//func: compare the user choice and computer choice to determine winner 
+
+// allert declare winner 
+
+// log who won 
+
+// prompt the user to see if they want to play again 
+
+// also include stats 
+
+
+
+
+
+
+
+
+
+
